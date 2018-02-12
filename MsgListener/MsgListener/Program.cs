8decc5741562f7614a59b62906ce67cfe0cbec58
Nasutya <nastia@bratchikovy.ru>
2018-02-12 17:36:53 +0500

    Add example for adapters

---
+++ MsgListener/MsgListener/Program.cs
@@ -0,0 +1,109 @@
+﻿namespace MsgListener
+{
+    using System;
+    using System.Configuration;
+    using System.ServiceModel;
+    using System.Threading;
+    using ICSSoft.STORMNET.Tools;
+    using ServiceBusServiceClient;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var messageFromESB = new MessageFromESB();
+            MyServiceHost.StartService();
+
+            Console.WriteLine("MsgListener started.");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+            MyServiceHost.StopService();
+        }
+
+        internal class MyServiceHost
+        {
+            private static Thread ScanThread;
+
+            internal static ServiceHost myServiceHost = null;
+
+            internal static void StartService()
+            {
+                myServiceHost = new ServiceHost(typeof(MsgListenerClass));
+
+                myServiceHost.Open();
+
+                ScanThread = MsgListenerClass.GetScanningThread();
+                ScanThread.Start();
+            }
+
+            internal static void StopService()
+            {
+                ScanThread.Abort();
+
+                if (myServiceHost.State != CommunicationState.Closed)
+                    myServiceHost.Close();
+            }
+        }
+
+        [ServiceContract]
+        public interface ICallbackSubscriber
+        {
+            [OperationContract]
+            void AcceptMessage(MessageFromESB msg);
+
+            [OperationContract]
+            void RiseEvent(string ИдТипаСобытия);
+
+            string GetSourceId();
+        }
+
+        public class MsgListenerClass : ICallbackSubscriber
+        {
+            public string GetSourceId()
+            {
+                return ConfigurationManager.AppSettings["ExternalKey"];
+            }
+
+            public void AcceptMessage(MessageFromESB msg)
+            {
+                Console.WriteLine(ToolZIP.Decompress(msg.Body));
+            }
+
+            public void RiseEvent(string ИдТипаСобытия)
+            {
+                Console.WriteLine(ИдТипаСобытия);
+            }
+
+            internal static void SubscribeMe4Messages(string ИдТипаСообщения)
+            {
+                using (ServiceBusServiceClient.ServiceBusServiceClient ServiceBus = new
+                ServiceBusServiceClient.ServiceBusServiceClient())
+                {
+                    ServiceBus.SubscribeClientForMessageCallback(
+                    ConfigurationManager.AppSettings["ServiceID4SB"],
+                    ИдТипаСообщения);
+
+                    ServiceBus.Close();
+                }
+            }
+
+            private static void NewSubscribeOrUpdate()
+            {
+                while (true)
+                {
+                    SubscribeMe4Messages(
+                    ConfigurationManager.AppSettings["MessageTypeID"]);
+
+                    Thread.Sleep(Convert.ToInt32(
+                    ConfigurationManager.AppSettings["ScanPeriod"]));
+                }
+            }
+
+            public static Thread GetScanningThread()
+            {
+                return new Thread(NewSubscribeOrUpdate);
+            }
+        }
+    }
+}
---
+++ MsgSender/MsgSender/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace MsgSender
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Configuration;
+    using ICSSoft.STORMNET.Tools;
+    using System.Net;
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "";
+
+            while (s != "exit")
+            {
+                Console.WriteLine("Enter your name (for exit type \"exit\"):");
+
+                s = Console.ReadLine();
+
+                if (s != "exit")
+                {
+                    using (var ServiceBus = new ServiceBusServiceClient("SBService"))
+                    {
+                        // Установим прокси, если нужно.
+                        var useProxy = ConfigurationManager.AppSettings["UseProxy"];
+                        if (!string.IsNullOrEmpty(useProxy) && useProxy.ToLower() == "true")
+                        {
+                            var proxy = new WebProxy(ConfigurationManager.AppSettings["ProxyServer"], true)
+                            {
+                                Credentials = new NetworkCredential(ConfigurationManager.AppSettings["ProxyLogin"], ConfigurationManager.AppSettings["ProxyPass"])
+                            };
+                            WebRequest.DefaultWebProxy = proxy;
+                        }
+
+                        // Создадим сообщение.
+                        var message = new MessageForESB
+                        {
+                            ClientID = ConfigurationManager.AppSettings["ServiceID4SB"],
+                            MessageTypeID = ConfigurationManager.AppSettings["MessageTypeID"],
+                            Body = ToolZIP.Compress("Hello from " + s + "!")
+                        };
+
+                        // И отправим его через шину.
+                        ServiceBus.SendMessageToESB(message);
+
+                        ServiceBus.Close();
+                    }
+                }
+            }
+
+        }
+    }
+}
