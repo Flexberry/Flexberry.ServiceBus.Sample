d12b6acaeb1546664df9d27ce0af1dee3fdcfb67
PashaMasalkin <pashamasalkin@yandex.ru>
2018-02-27 16:30:22 +0500

    Add Rest adapter example

---
+++ RESTAdapter/MsgRecipient/MsgRecipient/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Configuration;
+
+namespace MsgRecipient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string url = ConfigurationManager.AppSettings["Address"] + "/Messages?clientId=" + ConfigurationManager.AppSettings["RecipientID"];
+
+                using (var webClient = new WebClient())
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    var response = webClient.DownloadString(url);
+                    Console.WriteLine(response);
+                }
+
+                Thread.Sleep(5000);
+            }
+        }
+    }
+}
---
+++ RESTAdapter/MsgSender/MsgSender/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Configuration;
+using Newtonsoft.Json;
+
+namespace MsgSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string msgBody = "";
+
+            while (msgBody != "exit")
+            {
+                Console.Write("Enter your msg (for exit type \"exit\"): ");
+
+                msgBody = Console.ReadLine();
+
+                // Создадим сообщение.
+                string jsonData = JsonConvert.SerializeObject(new
+                {
+                    Body = msgBody,
+                    ClientID = ConfigurationManager.AppSettings["SenderID"],
+                    MessageTypeID = ConfigurationManager.AppSettings["MessageTypeID"]
+                });
+
+                string url = ConfigurationManager.AppSettings["Address"] + "/Message";
+
+                using (var webClient = new WebClient())
+                {
+                    webClient.Encoding = Encoding.UTF8;
+                    webClient.Headers[HttpRequestHeader.ContentType] = "application/json";
+                    webClient.UploadString(url, "POST", jsonData);
+                    Console.WriteLine("Message sent");    
+                }
+            }
+        }
+    }
+}
