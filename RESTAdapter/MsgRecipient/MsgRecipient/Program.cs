da94f5ee5a2f8aa973994c72844d49a2fac136b7
akosinsky <akosinsky@ICS-KK12324>
2018-03-05 18:30:39 +0500

    Fix REST example

--- RESTAdapter/MsgRecipient/MsgRecipient/Program.cs
+++ RESTAdapter/MsgRecipient/MsgRecipient/Program.cs
@@ -3,6 +3,9 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Configuration;
+using Newtonsoft.Json;
+using System.IO;
+using System.Collections.Generic;
 
 namespace MsgRecipient
 {
@@ -18,11 +21,36 @@ namespace MsgRecipient
                 {
                     webClient.Encoding = Encoding.UTF8;
                     var response = webClient.DownloadString(url);
-                    Console.WriteLine(response);
+                    List<MessageListItem> items = JsonConvert.DeserializeObject<List<MessageListItem>>(response);
+                    foreach (var item in items)
+                    {
+                        string url2 = ConfigurationManager.AppSettings["Address"] + "/Message/" + item.Id;
+                        response = webClient.DownloadString(url2);
+                        Message msg = JsonConvert.DeserializeObject<Message>(response);
+                        Console.WriteLine(response);
+                    }
                 }
 
                 Thread.Sleep(5000);
             }
         }
     }
+
+    public class MessageListItem
+    {
+        public string Id { get; set; }
+        public string MessageTypeID { get; set; }
+        public int Priority { get; set; }
+        public string MessageFormingTime { get; set; }
+    }
+
+    public class Message
+    {
+        public string Id { get; set; }
+        public string Body { get; set; }
+        public string MessageFormingTime { get; set; }
+        public string MessageTypeID { get; set; }
+        public string SenderName { get; set; }
+    }
+
 }
